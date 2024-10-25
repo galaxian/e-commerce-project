6b7ba26b203e7c6f11f85b8786bd9786a5ed0288
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-25 21:24:57 +0900

    feat: member 정보 암호화 로직 구현

--- src/main/java/com/example/ecommerce/member/application/MemberService.java
+++ src/main/java/com/example/ecommerce/member/application/MemberService.java
@@ -1,9 +1,16 @@
 package com.example.ecommerce.member.application;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.encrypt.BytesEncryptor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.ecommerce.member.application.dto.req.AddressDto;
+import com.example.ecommerce.member.application.dto.req.PhoneNumberDto;
 import com.example.ecommerce.member.application.dto.req.SignUpReqDto;
 import com.example.ecommerce.member.application.port.in.SignUpUseCase;
 import com.example.ecommerce.member.application.port.out.MemberRepository;
@@ -16,22 +23,53 @@ import com.example.ecommerce.member.domain.PhoneNumber;
 public class MemberService implements SignUpUseCase {
 
 	private final MemberRepository memberRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final BytesEncryptor bytesEncryptor;
 
 	@Autowired
-	public MemberService(MemberRepository memberRepository) {
+	public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder,
+		BytesEncryptor bytesEncryptor) {
 		this.memberRepository = memberRepository;
+		this.passwordEncoder = passwordEncoder;
+		this.bytesEncryptor = bytesEncryptor;
 	}
 
 	@Override
 	public Long signUp(SignUpReqDto reqDto) {
-		Address address = new Address(reqDto.addressDto().state(), reqDto.addressDto().street(),
-			reqDto.addressDto().city(),
-			reqDto.addressDto().zipCode());
-		PhoneNumber phoneNumber = new PhoneNumber(reqDto.phoneNumberDto().countryCode(),
-			reqDto.phoneNumberDto().areaCode(), reqDto.phoneNumberDto()
-			.number());
-
-		Member member = reqDto.toMember(address, phoneNumber);
+		Member member = createEncryptMember(reqDto);
 		return memberRepository.save(member).getId();
+	}
+
+	private Member createEncryptMember(SignUpReqDto reqDto) {
+		String encryptPassword = passwordEncoder.encode(reqDto.password());
+		Address address = createEncryptAddress(reqDto.addressDto());
+		PhoneNumber phoneNumber = createEncryptPhoneNumber(reqDto.phoneNumberDto());
+
+		String encryptEmail = encrypt(reqDto.email());
+		String encryptName = encrypt(reqDto.name());
+
+		return new Member(encryptEmail, encryptName, encryptPassword, address, phoneNumber);
+	}
+
+	private Address createEncryptAddress(AddressDto addressDto) {
+		String encryptState = encrypt(addressDto.state());
+		String encryptCity = encrypt(addressDto.city());
+		String encryptStreet = encrypt(addressDto.street());
+		String encryptZipCode = encrypt(addressDto.zipCode());
+
+		return new Address(encryptState, encryptStreet, encryptCity, encryptZipCode);
+	}
+
+	private PhoneNumber createEncryptPhoneNumber(PhoneNumberDto phoneNumberDto) {
+		String encryptCountryCode = encrypt(phoneNumberDto.countryCode());
+		String encryptAreaCode = encrypt(phoneNumberDto.areaCode());
+		String encryptNumber = encrypt(phoneNumberDto.number());
+
+		return new PhoneNumber(encryptCountryCode, encryptAreaCode, encryptNumber);
+	}
+
+	private String encrypt(String raw) {
+		byte[] encryptedBytes = bytesEncryptor.encrypt(raw.getBytes(StandardCharsets.UTF_8));
+		return Base64.getEncoder().encodeToString(encryptedBytes);
 	}
 }
