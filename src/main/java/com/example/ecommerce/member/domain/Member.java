2726ab2eeedb0de3b397f2878e2f23d706a29653
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-25 14:18:40 +0900

    feat: member 도메인 생성자 및 Equal & hashCode 오버라이드

--- src/main/java/com/example/ecommerce/member/domain/Member.java
+++ src/main/java/com/example/ecommerce/member/domain/Member.java
@@ -1,14 +1,26 @@
 package com.example.ecommerce.member.domain;
 
+import java.time.LocalDateTime;
+
 import lombok.Getter;
 
 @Getter
 public class Member {
 
 	private Long id;
-	private String email;
-	private String name;
+	private String encryptEmail;
+	private String encryptName;
 	private String encryptPassword;
 	private Address address;
 	private PhoneNumber phoneNumber;
+	private LocalDateTime createAt;
+	private LocalDateTime updateAt;
+
+	public Member(String encryptEmail, String encryptName, String encryptPassword, Address address, PhoneNumber phoneNumber) {
+		this.encryptEmail = encryptEmail;
+		this.encryptName = encryptName;
+		this.encryptPassword = encryptPassword;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+	}
 }
