c855257e559b771a494db8131e725379a8e22bf2
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-25 14:36:29 +0900

    feat: 회원가입 영속성 계층 구현

---
+++ src/main/java/com/example/ecommerce/member/infrastructure/repository/MemberRepositoryImpl.java
@@ -0,0 +1,23 @@
+package com.example.ecommerce.member.infrastructure.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.ecommerce.member.application.port.out.memberRepository;
+import com.example.ecommerce.member.domain.Member;
+import com.example.ecommerce.member.infrastructure.entity.MemberEntity;
+
+@Repository
+public class MemberRepositoryImpl implements memberRepository {
+
+	private final MemberJpaRepository memberJpaRepository;
+
+	public MemberRepositoryImpl(MemberJpaRepository memberJpaRepository) {
+		this.memberJpaRepository = memberJpaRepository;
+	}
+
+	@Override
+	public Member save(Member member) {
+		MemberEntity memberEntity = MemberEntity.from(member);
+		return memberJpaRepository.save(memberEntity).toMemberDomain();
+	}
+}
