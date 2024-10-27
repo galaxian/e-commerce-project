f82e2f71589d42d632b6bc1a6e5f67fe05e175b6
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-28 01:42:30 +0900

    test: springbootTest 제거

--- src/test/java/com/example/ecommerce/ECommerceApplicationTests.java
+++ src/test/java/com/example/ecommerce/ECommerceApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class ECommerceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
