c423e7fa9d27796b44579b5cefd73efe6b83462d
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-28 15:01:51 +0900

    feat: email로 사용자 조회 기능 구현

--- src/main/java/com/example/ecommerce/member/infrastructure/repository/MemberJpaRepository.java
+++ src/main/java/com/example/ecommerce/member/infrastructure/repository/MemberJpaRepository.java
@@ -1,8 +1,12 @@
 package com.example.ecommerce.member.infrastructure.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.ecommerce.member.domain.Member;
 import com.example.ecommerce.member.infrastructure.entity.MemberEntity;
 
 public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long> {
+	Optional<MemberEntity> findByEncryptEmail(String encryptEmail);
 }
