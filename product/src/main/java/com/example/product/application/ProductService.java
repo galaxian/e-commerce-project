983f3fee148a46fc7335544cc4984dd9a0b0a935
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-30 02:05:10 +0900

    feat : 상품 상세 조회 서비스 로직 구현

--- product/src/main/java/com/example/product/application/ProductService.java
+++ product/src/main/java/com/example/product/application/ProductService.java
@@ -6,13 +6,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.product.application.dto.res.FindAllProductResDto;
+import com.example.product.application.dto.res.FindProductResDto;
 import com.example.product.application.port.in.FindAllProductUseCase;
+import com.example.product.application.port.in.FindProductUseCase;
 import com.example.product.application.port.out.ProductRepository;
+import com.example.product.common.exception.ProductNotFoundException;
 import com.example.product.domain.Product;
 
 @Transactional
 @Service
-public class ProductService implements FindAllProductUseCase {
+public class ProductService implements FindAllProductUseCase, FindProductUseCase {
 
 	private final ProductRepository productRepository;
 
@@ -25,6 +28,28 @@ public class ProductService implements FindAllProductUseCase {
 	public List<FindAllProductResDto> findAllProduct() {
 		List<Product> productList = productRepository.findAll();
 		return convertToDto(productList);
+	}
+
+	@Transactional(readOnly = true)
+	@Override
+	public FindProductResDto findProduct(Long productId) {
+		Product product = getProductById(productId);
+		return convertToDto(product);
+	}
+
+	private Product getProductById(Long productId) {
+		return productRepository.findById(productId)
+			.orElseThrow(() -> new ProductNotFoundException("상품을 찾을 수 없습니다."));
+	}
+
+	private FindProductResDto convertToDto(Product product) {
+		return new FindProductResDto(
+			product.getId(),
+			product.getName(),
+			product.getDescription(),
+			product.getPrice(),
+			product.getStock()
+		);
 	}
 
 	private static List<FindAllProductResDto> convertToDto(List<Product> productList) {
