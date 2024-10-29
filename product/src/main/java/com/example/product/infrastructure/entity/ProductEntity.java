0dc2ba980a1638e07f32f6589d74ee3fe7c0f6b5
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-30 00:54:31 +0900

    feat : 상품 jpa entity 작성

---
+++ product/src/main/java/com/example/product/infrastructure/entity/ProductEntity.java
@@ -0,0 +1,38 @@
+package com.example.product.infrastructure.entity;
+
+import java.math.BigDecimal;
+
+import com.example.product.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "product")
+public class ProductEntity extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "description", nullable = false)
+	private String description;
+
+	@Column(name = "price", nullable = false)
+	private BigDecimal price;
+
+	@Column(name = "stock", nullable = false)
+	private Integer stock;
+}
