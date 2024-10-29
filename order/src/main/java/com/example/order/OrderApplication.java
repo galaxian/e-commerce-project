b6725792e1404c83bf81f237cedff2c847fbd7b3
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-29 19:47:05 +0900

    build : order 모듈에서 auth 모듈 스캔

--- order/src/main/java/com/example/order/OrderApplication.java
+++ order/src/main/java/com/example/order/OrderApplication.java
@@ -3,7 +3,7 @@ package com.example.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.auth", "com.example.order"})
 public class OrderApplication {
 
 	public static void main(String[] args) {
