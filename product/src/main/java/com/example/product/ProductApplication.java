90f486ddae18deee3143ebfeb7098109c4923127
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-29 18:19:20 +0900

    build : product component scan에 auth 모듈 추가

--- product/src/main/java/com/example/product/ProductApplication.java
+++ product/src/main/java/com/example/product/ProductApplication.java
@@ -3,7 +3,7 @@ package com.example.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.auth", "com.example.product"})
 public class ProductApplication {
 
 	public static void main(String[] args) {
