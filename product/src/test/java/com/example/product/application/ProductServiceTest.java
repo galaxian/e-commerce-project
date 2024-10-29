193cc6f25f34a126b4f890494ae6d05aac703186
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-30 01:31:54 +0900

    test : 상품 목록 조회 성공 단위 테스트

--- product/src/test/java/com/example/product/application/ProductServiceTest.java
+++ product/src/test/java/com/example/product/application/ProductServiceTest.java
@@ -3,6 +3,8 @@ package com.example.product.application;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.example.product.application.dto.res.FindAllProductResDto;
 import com.example.product.application.port.out.ProductRepository;
+import com.example.product.domain.Product;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
@@ -39,6 +42,31 @@ class ProductServiceTest {
 		assertThat(result).isEmpty();
 		then(productRepository).should(times(1)).findAll();
 
+	}
+
+	@DisplayName("상품 목록 조회")
+	@Test
+	public void findAllProduct() {
+		// given
+		Product product1 = new Product(1L, "상품1", "상품설명1", new BigDecimal(10000), 10, null, null);
+		Product product2 = new Product(2L, "상품2", "상품설명2", new BigDecimal(20000), 20, null, null);
+		List<Product> productList = Arrays.asList(product1, product2);
+
+		given(productRepository.findAll()).willReturn(productList);
+
+		// when
+		List<FindAllProductResDto> result = productService.findAllProduct();
+
+		// then
+		assertThat(result)
+			.hasSize(productList.size())
+			.extracting(FindAllProductResDto::name)
+			.containsExactly("상품1", "상품2");
+		assertThat(result)
+			.extracting(FindAllProductResDto::price)
+			.containsExactly(new BigDecimal("10000"), new BigDecimal("20000"));
+
+		then(productRepository).should(times(1)).findAll();
 	}
 
 }
