ded941bdd48aa869374686439b71d87d7e373eff
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-30 02:04:25 +0900

    feat : 상품 상세 조회 in/out 인터페이스 작성

--- product/src/main/java/com/example/product/application/port/out/ProductRepository.java
+++ product/src/main/java/com/example/product/application/port/out/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.example.product.application.port.out;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.product.domain.Product;
 
 public interface ProductRepository {
 
 	List<Product> findAll();
+	Optional<Product> findById(Long id);
 }
