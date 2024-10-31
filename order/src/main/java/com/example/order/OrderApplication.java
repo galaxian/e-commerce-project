f761796d6858ad19ca4f253928b70b4e4274eb22
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-31 18:38:10 +0900

    feat : order 모듈 componentScan에 member, product 추가

--- order/src/main/java/com/example/order/OrderApplication.java
+++ order/src/main/java/com/example/order/OrderApplication.java
@@ -3,7 +3,7 @@ package com.example.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.example.auth", "com.example.order"})
+@SpringBootApplication(scanBasePackages = {"com.example.auth", "com.example.order", "com.example.member", "com.example.product"})
 public class OrderApplication {
 
 	public static void main(String[] args) {
