8cee4a1af2ac75317fa70102cf2875c3a39a655d
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-31 18:35:35 +0900

    feat : 주문 관련 jpa entity 구현 및 변환 로직 구현

--- order/src/main/java/com/example/order/domain/Order.java
+++ order/src/main/java/com/example/order/domain/Order.java
@@ -18,4 +18,25 @@ public class Order {
 	private LocalDateTime createAt;
 	private LocalDateTime updateAt;
 	private Member member;
+
+	public Order(BigDecimal totalAmount, Address shippingAddress, Member member) {
+		this.totalAmount = totalAmount;
+		this.orderStatus = OrderStatus.PENDING;
+		this.shippingAddress = shippingAddress;
+		this.orderDate = LocalDateTime.now();
+		this.member = member;
+	}
+
+	public Order(Long id, BigDecimal totalAmount, OrderStatus orderStatus, Address shippingAddress,
+		LocalDateTime orderDate,
+		LocalDateTime createAt, LocalDateTime updateAt, Member member) {
+		this.id = id;
+		this.totalAmount = totalAmount;
+		this.orderStatus = orderStatus;
+		this.shippingAddress = shippingAddress;
+		this.orderDate = orderDate;
+		this.createAt = createAt;
+		this.updateAt = updateAt;
+		this.member = member;
+	}
 }
--- order/src/main/java/com/example/order/domain/OrderItem.java
+++ order/src/main/java/com/example/order/domain/OrderItem.java
@@ -1,6 +1,7 @@
 package com.example.order.domain;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import com.example.product.domain.Product;
 
@@ -14,4 +15,24 @@ public class OrderItem {
 	private Integer quantity;
 	private Order order;
 	private Product product;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+
+	public OrderItem(BigDecimal price, Integer quantity, Order order, Product product) {
+		this.price = price;
+		this.quantity = quantity;
+		this.order = order;
+		this.product = product;
+	}
+
+	public OrderItem(Long id, BigDecimal price, Integer quantity, Order order, Product product, LocalDateTime createdAt,
+		LocalDateTime updatedAt) {
+		this.id = id;
+		this.price = price;
+		this.quantity = quantity;
+		this.order = order;
+		this.product = product;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
 }
