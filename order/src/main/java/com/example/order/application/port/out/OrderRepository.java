69bd4954882c496478b9828dba0c6bfc41c87c74
Lee <94841127+galaxian@users.noreply.github.com>
2024-11-04 19:08:58 +0900

    feat: OrderRepository Out Port 정의

--- order/src/main/java/com/example/order/application/port/out/OrderRepository.java
+++ order/src/main/java/com/example/order/application/port/out/OrderRepository.java
@@ -1,8 +1,12 @@
 package com.example.order.application.port.out;
 
+import java.util.List;
+
 import com.example.order.domain.Order;
 
 public interface OrderRepository {
 
 	Order save(Order order);
+
+	List<Order> findAllByMemberId(Long userId);
 }
