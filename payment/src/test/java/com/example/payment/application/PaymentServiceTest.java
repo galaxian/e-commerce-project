f5cf413ca741f8e360d33a899cadc4e479a0ab69
Lee <94841127+galaxian@users.noreply.github.com>
2024-11-11 17:46:53 +0900

    test: 결제 생성 성공 단위 테스트

---
+++ payment/src/test/java/com/example/payment/application/PaymentServiceTest.java
@@ -0,0 +1,75 @@
+package com.example.payment.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.member.application.port.out.MemberRepository;
+import com.example.member.domain.Member;
+import com.example.order.application.port.out.OrderRepository;
+import com.example.order.domain.Address;
+import com.example.order.domain.Order;
+import com.example.order.domain.OrderStatus;
+import com.example.payment.application.port.out.PaymentRepository;
+import com.example.payment.domain.Payment;
+import com.example.payment.domain.PaymentStatus;
+
+@ExtendWith(MockitoExtension.class)
+class PaymentServiceTest {
+
+	@Mock
+	private PaymentRepository paymentRepository;
+
+	@Mock
+	private OrderRepository orderRepository;
+
+	@Mock
+	private MemberRepository memberRepository;
+
+	@InjectMocks
+	private PaymentService paymentService;
+
+	@DisplayName("결제 생성 성공")
+	@Test
+	void createPayment() {
+		// Given
+		Long orderId = 1L;
+		Long memberId = 1L;
+
+		Member member = new Member(1L, "암호화 메일", "암호화 이름", "암호화 비밀번호",
+			new com.example.member.domain.Address("서울", "ㅁㅁㅁ", "강남", "12345"), null, null, null);;
+		Order order = new Order(1L, BigDecimal.valueOf(15000), OrderStatus.PENDING, new Address("서울", "ㅁㅁㅁ", "강남", "12345"),
+			LocalDateTime.now(), null, null, member);
+
+		Payment payment = new Payment(1L, BigDecimal.valueOf(15000), PaymentStatus.PENDING, null, null, order, member,
+			null, null);
+
+		given(orderRepository.findByOrderIdAndMemberId(orderId, memberId))
+			.willReturn(Optional.of(order));
+		given(memberRepository.findById(memberId))
+			.willReturn(Optional.of(member));
+		given(paymentRepository.save(any(Payment.class)))
+			.willReturn(payment);
+
+		// When
+		Long result = paymentService.createPayment(orderId, memberId);
+
+		// Then
+		assertThat(result).isNotNull();
+		assertThat(result).isEqualTo(payment.getId());
+		then(orderRepository).should(times(1)).findByOrderIdAndMemberId(orderId, memberId);
+		then(memberRepository).should(times(1)).findById(memberId);
+		then(paymentRepository).should(times(1)).save(any(Payment.class));
+	}
+}
