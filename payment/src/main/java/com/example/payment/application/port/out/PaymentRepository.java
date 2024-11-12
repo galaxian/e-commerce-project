f6ef02a0052a65c4c255a435abf1a3508c7ce7bb
Lee <94841127+galaxian@users.noreply.github.com>
2024-11-12 09:09:44 +0900

    feat: 결제 진행에 필요한 in/out port 정의

--- payment/src/main/java/com/example/payment/application/port/out/PaymentRepository.java
+++ payment/src/main/java/com/example/payment/application/port/out/PaymentRepository.java
@@ -1,7 +1,11 @@
 package com.example.payment.application.port.out;
 
+import java.util.Optional;
+
 import com.example.payment.domain.Payment;
 
 public interface PaymentRepository {
 	Payment save(Payment payment);
+
+	Optional<Payment> findById(Long paymentId);
 }
