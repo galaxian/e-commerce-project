c6c2c1924e03f002506a01dbf7bb8b6b4786ead8
Lee <94841127+galaxian@users.noreply.github.com>
2024-11-12 10:41:14 +0900

    test: 결제 처리 단위 테스트

--- payment/src/test/java/com/example/payment/domain/PaymentTest.java
+++ payment/src/test/java/com/example/payment/domain/PaymentTest.java
@@ -58,4 +58,25 @@ class PaymentTest {
 
 	}
 
+	@DisplayName("결제 처리 성공")
+	@Test
+	void successPayment() {
+		//given
+		Member member = new Member(1L, "암호화 메일", "암호화 이름", "암호화 비밀번호",
+			new com.example.member.domain.Address("서울", "ㅁㅁㅁ", "강남", "12345"), null, null, null);;
+		Order order = new Order(1L, BigDecimal.valueOf(15000), OrderStatus.PENDING, new Address("서울", "ㅁㅁㅁ", "강남", "12345"),
+			LocalDateTime.now(), null, null, member);
+
+		Payment payment = new Payment(1L, BigDecimal.valueOf(15000), PaymentStatus.PENDING, null, null, order, member,
+			null, null);
+
+		//when
+		payment.successPayment();
+
+		//then
+		assertThat(payment.getPaymentStatus()).isEqualTo(PaymentStatus.SUCCESS);
+		assertThat(payment.getPaymentMethod()).isEqualTo(PaymentMethod.CARD);
+		assertThat(payment.getPaymentDate()).isNotNull();
+
+	}
 }
